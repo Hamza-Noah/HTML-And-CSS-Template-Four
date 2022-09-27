daafa5cdf81f1645123df14f05eed5cc8342e6d7
Hamza-Noah <hamza.nouh97@gmail.com>
2022-09-27 13:07:59 +0200

    added the project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SetteingsComponent } from './setteings/setteings.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FilesComponent } from './files/files.component';
+import { PlansComponent } from './plans/plans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SetteingsComponent,
+    ProfileComponent,
+    ProjectsComponent,
+    CoursesComponent,
+    FriendsComponent,
+    FilesComponent,
+    PlansComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/setteings/setteings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setteings',
+  templateUrl: './setteings.component.html',
+  styleUrls: ['./setteings.component.scss']
+})
+export class SetteingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
