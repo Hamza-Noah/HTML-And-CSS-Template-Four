fcd72eef5ef51a1dea764e3e1c3f88434f38738e
Hamza-Noah <hamza.nouh97@gmail.com>
2022-10-23 14:25:44 +0200

    some simple fixes for styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { SetteingsComponent } from './setteings/setteings.component';
 
 const routes: Routes = [
   {path: "dashboard", component: DashboardComponent},
-  {path: "settings", component: SetteingsComponent}
+  {path: "settings", component: SetteingsComponent},
+  {path: "profile", component: ProfilePageComponent}
 ];
 
 @NgModule({
