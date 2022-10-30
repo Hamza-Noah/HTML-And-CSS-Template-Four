781a9a8c060e0b32628b270ebc892c5f37acd6fe
Hamza-Noah <hamza.nouh97@gmail.com>
2022-10-30 10:12:30 +0200

    added the latest uploads interface

--- src/app/dashboard/latest-uploads/latest-uploads.component.ts
+++ src/app/dashboard/latest-uploads/latest-uploads.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LatestUploads } from "../../interfaces/latest-uploads"
 @Component({
   selector: 'app-latest-uploads',
   templateUrl: './latest-uploads.component.html',
   styleUrls: ['./latest-uploads.component.scss']
 })
 export class LatestUploadsComponent implements OnInit {
+
+
+  imgPath: string = "assets/images/";
+  uploads: LatestUploads[] = [
+    { imgSrc: "pdf.svg", title: "my-file.pdf", userName: "Elzero", size: 2.9 },
+    { imgSrc: "avi.svg", title: "My-Video-File.avi", userName: "Elzero", size: 4.9 },
+    { imgSrc: "psd.svg", title: "My-Psd-File.pdf", userName: "Admin", size: 4.5 },
+    { imgSrc: "zip.svg", title: "My-Zip-File.pdf", userName: "Osama", size: 8.9 },
+    { imgSrc: "dll.svg", title: "My-DLL-File.pdf", userName: "User", size: 4.9 },
+    { imgSrc: "eps.svg", title: "my-Eps-file.pds", userName: "Designer", size: 8.9 },
+  ]
 
   constructor() { }
 
---
+++ src/app/interfaces/latest-uploads.ts
@@ -0,0 +1,6 @@
+export interface LatestUploads {
+    imgSrc: string,
+    title: string;
+    userName: string;
+    size: number;
+}
