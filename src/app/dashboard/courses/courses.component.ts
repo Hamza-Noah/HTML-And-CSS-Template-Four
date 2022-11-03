98d0f3040536836921902ae5a21d8e8ca7e1e44d
Hamza-Noah <hamza.nouh97@gmail.com>
2022-11-03 16:49:41 +0200

    added the courses interface

--- src/app/dashboard/courses/courses.component.ts
+++ src/app/dashboard/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Courses } from 'src/app/interfaces/courses';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  course: Courses[] = [
+    {
+      cover: "course-01.jpg",
+      image: "team-01.png",
+      title: "Mastering Web Design",
+      details: "Master The Art Of Web Designing And Mocking, Prototyping And Creating Web Design Architecture",
+      attendees: 950,
+      price: 165
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/interfaces/courses.ts
@@ -0,0 +1,9 @@
+export interface Courses {
+    cover: string;
+    image: string;
+    title: string;
+    details: string;
+    attendees: number;
+    price: number
+
+}
