7d5ce74d8a1ba6c99dc10adc7c3e1827a4f440f2
Hamza-Noah <hamza.nouh97@gmail.com>
2022-11-13 11:25:28 +0200

    added the image of every course

--- src/app/dashboard/courses/courses.component.ts
+++ src/app/dashboard/courses/courses.component.ts
@@ -50,6 +50,46 @@ export class CoursesComponent implements OnInit {
       attendees: 950,
       price: 250
     },
+    {
+      cover: "course-01.jpg",
+      image: "team-01.png",
+      title: "Mastering Web Design",
+      details: "Master The Art Of Web Designing And Mocking, Prototyping And Creating Web Design Architecture",
+      attendees: 950,
+      price: 165
+    },
+    {
+      cover: "course-02.jpg",
+      image: "team-02.png",
+      title: "Data Structure And Algorithms",
+      details: "",
+      attendees: 1150,
+      price: 210
+    },
+    {
+      cover: "course-03.jpg",
+      image: "team-03.png",
+      title: "Responsive Web Design",
+      details: "",
+      attendees: 1150,
+      price: 210
+    },
+    {
+      cover: "course-04.jpg",
+      image: "team-04.png",
+      title: "Mastering Python",
+      details: "",
+      attendees: 950,
+      price: 250
+    },
+    {
+      cover: "course-05.jpg",
+      image: "team-05.png",
+      title: "Mastering Python",
+      details: "",
+      attendees: 950,
+      price: 250
+    },
 
   ]
 
