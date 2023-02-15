6820bb931a39c0432225d2e3e44edfb1e729ba37
Hamza-Noah <hamza.nouh97@gmail.com>
2023-02-15 15:27:21 +0200

    added the files page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './dashboard/courses/courses.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FriendsComponent } from './dashboard/friends/friends.component';
+import { FilesPageComponent } from './files-page/files-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { SettingsPageComponent } from './settings-page/settings-page.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: "projects", component: ProjectsPageComponent },
   { path: "courses", component: CoursesComponent },
   { path: "friends", component: FriendsComponent },
+  { path: "files", component: FilesPageComponent },
   { path: "**", redirectTo: "dashboard" }
 ];
 
